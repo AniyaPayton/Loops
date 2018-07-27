8882656e7617c5f211a5e88ccf0acd106e308dd1
DePaul <CONF0201@depaul.edu>
2018-07-27 15:54:07 -0500

    Loops

---
+++ Loops_script.js
@@ -0,0 +1,19 @@
+ $(document).ready(function() {
+	 
+	 console.log("hello");
+	  
+	  
+	var musicals_array = ["Wicked","Hamilton", "Once on This Island", "The Color Purple", "In the Heights", "Chicago",
+	"Hairspray"];
+	var length= musicals_array.length
+	musicals_array.sort();
+	musicals_array.reverse();
+	console.log(musicals_array);
+	console.log(length)
+	
+	
+		
+		
+		
+	});
+ 
